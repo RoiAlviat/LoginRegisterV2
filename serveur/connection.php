7a68f0decb2be1a6314cce3b81f6405884f97590
AeRyZe <laurent.charleval@free.fr>
2022-11-16 23:21:16 +0100

    update 0.1

--- serveur/connection.php
+++ serveur/connection.php
@@ -8,7 +8,7 @@ define('PASS', '');
 try {
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connexion -> RÃ©ussie'; 
+    echo 'Connexion réussie !'.'<br/>'; 
 } catch(PDOException $e) {
     echo $e;
 
